fcba52ffa0f71afaa7d330146bbe9bacc6081ac0
SamAng219255 <SamAng033146085@gmail.com>
2025-11-06 14:21:12 -0800

    Fompleted "Team Activity W02: Dynamic product detail pages"

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,8 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
-const cart = getLocalStorage('so-cart') || [];
+const product = new ProductDetails(getParam('product'), dataSource);
 
-function addProductToCart(product) {
-  cart.push(product);
-  setLocalStorage('so-cart', cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+product.init();
