c372572d73ff10c8f61d78d06657c8f1aedf53fd
EliMAmy <rociomarapaza@gmail.com>
2025-11-06 15:11:15 -0500

    cart fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,14 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData('tents');
-const cart = getLocalStorage('so-cart') || [];
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  cart.push(product);
-  setLocalStorage('so-cart', cart);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -16,5 +17,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
