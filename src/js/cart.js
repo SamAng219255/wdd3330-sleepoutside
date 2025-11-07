edc20aa726f45837177efe1f099f9c3281be3c5b
SamAng219255 <SamAng033146085@gmail.com>
2025-11-06 21:24:27 -0800

    Added remove from cart feature.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,25 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+
+const cartItems = getLocalStorage('so-cart') || [];
 
 function renderCartContents() {
-  const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+  document.querySelectorAll(".cart-card__close")
+    .forEach(
+      btn => btn.addEventListener(
+        "click",
+        e => removeFromCart(btn.dataset.id)
+      )
+    );
+}
+
+function removeFromCart(id) {
+  cartItems.splice(cartItems.findIndex(item => item.Id == id), 1);
+  setLocalStorage('so-cart', cartItems);
+
+  renderCartContents();
 }
 
 function cartItemTemplate(item) {
@@ -20,6 +36,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="cart-card__close" data-id="${item.Id}">X</button>
 </li>`;
 
   return newItem;
