580d5e53cecc4882fe922d879b52e2a2d30942bb
SamAng219255 <SamAng033146085@gmail.com>
2025-11-14 17:27:33 -0800

    Fixed string terminator

--- src/js/cart_label.mjs
+++ src/js/cart_label.mjs
@@ -1,19 +1,21 @@
 import { getLocalStorage, qs } from './utils.mjs';
 
 export default function setCartLabel(value) {
-	if(value == undefined) {
-		value = (getLocalStorage('so-cart') || []).length;
+	let count = value;
+	
+	if(count == undefined) {
+		count = (getLocalStorage('so-cart') || []).length;
 	}
 
 	const cartLabel = qs('.cart span');
 
-	cartLabel.innerText = value;
+	cartLabel.innerText = count;
 
-	if(value <= 0) cartLabel.classList.remove('show');
+	if(count <= 0) cartLabel.classList.remove('show');
 	else cartLabel.classList.add('show');
 }
 
-window.addEventListener("storage", (event) => {
+window.addEventListener('storage', (event) => {
 	if(event.storageArea === localStorage && event.key == 'so-cart')
 		setCartLabel();
 });
