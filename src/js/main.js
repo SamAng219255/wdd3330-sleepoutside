0e2fc7a266065933b4d45cdbbd205db044c8c402
SamAng219255 <SamAng033146085@gmail.com>
2025-11-15 10:18:12 -0800

    Completed w03 team activity: Dynamic header and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
-import { qs } from './utils.mjs';
+import { qs, loadHeaderFooter } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 
 const dataSource = new ProductData('tents');
 const products = new ProductList('tents', dataSource, qs('.product-list'));
 products.init();
+
+loadHeaderFooter();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,5 @@
+import setCartLabel from './cart_label.mjs';
+
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
@@ -37,4 +39,28 @@ export function renderListWithTemplate(
 ) {
   if (clear) parentElement.innerHTML = '';
   parentElement.insertAdjacentHTML(position, list.map(templateFn).join('\n'));
+}
+// renders an html template
+export function renderWithTemplate(template, parentElement, callback, data) {
+  parentElement.innerHTML = template;
+
+  if(callback !== undefined)
+    callback(data);
+}
+// fetches a template from the given path
+async function loadTemplate(path) {
+  const response = await fetch(path);
+  const text = await response.text();
+  return text;
+}
+// fetches and loads the header and footer
+export async function loadHeaderFooter() {
+  const header = await loadTemplate('/partials/header.html');
+  const footer = await loadTemplate('/partials/footer.html');
+
+  const headerElem = document.getElementById('main-header');
+  const footerElem = document.getElementById('main-footer');
+
+  renderWithTemplate(header, headerElem, setCartLabel);
+  renderWithTemplate(footer, footerElem, setCartLabel);
 }
