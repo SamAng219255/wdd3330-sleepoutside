69d961beb210ffb7a4750865b60200c1b23e8421
SamAng219255 <SamAng033146085@gmail.com>
2025-11-14 16:20:49 -0800

    Completed Individual Activity W03: Expand Inventory.

---
+++ src/js/product-listing.js
@@ -0,0 +1,18 @@
+import { qs, getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
+const category = getParam('category');
+const dataSource = new ProductData();
+const products = new ProductList(category, dataSource, qs('.product-list'));
+products.init();
+
+const categoryNames = {
+  tents: 'Tents',
+  backpacks: 'Backpacks',
+  'sleeping-bags': 'Sleeping Bags',
+  hammocks: 'Hammocks',
+};
+if (category in categoryNames) {
+  qs('h2').innerText = `Top Products: ${categoryNames[category]}`;
+}
