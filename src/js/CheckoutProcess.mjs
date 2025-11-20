74c44fbd27247250acd136c603dcb5db3bb315f2
SamAng219255 <SamAng033146085@gmail.com>
2025-11-20 13:02:58 -0800

    Additional bug fixes.

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -5,9 +5,9 @@ export default class CheckoutProcess {
   }
 
   displaySubtotal() {
-    this.subtotal = this.cartItems.reduce((acc, item) => acc + item.FinalPrice, 0);
+    const subtotal = this.cartItems.reduce((acc, item) => acc + item.FinalPrice, 0);
     this.summaryElem.innerHTML = `
-      <p>Subtotal: $${this.subtotal.toFixed(2)}</p>
+      <p>Subtotal: $${subtotal.toFixed(2)}</p>
       <p>Tax: —</p>
       <p>Shipping: —</p>
       <hr>
@@ -16,11 +16,12 @@ export default class CheckoutProcess {
   }
 
   displayTotal() {
-    const tax = this.subtotal * 0.06;
+    const subtotal = this.cartItems.reduce((acc, item) => acc + item.FinalPrice, 0);
+    const tax = subtotal * 0.06;
     const shipping = 8 + 2 * this.cartItems.length;
-    const total = this.subtotal + tax + shipping;
+    const total = subtotal + tax + shipping;
     this.summaryElem.innerHTML = `
-      <p>Subtotal: $${this.subtotal.toFixed(2)}</p>
+      <p>Subtotal: $${subtotal.toFixed(2)}</p>
       <p>Tax: $${tax.toFixed(2)}</p>
       <p>Shipping: $${shipping.toFixed(2)}</p>
       <hr>
